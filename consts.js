87891706bd3ecf32f77f1f01c8c98183daeca87a
Anders Svensson <andess86@gmail.com>
2020-03-14 12:45:37 +0100

    Move filename and url to consts file, set up daily fetch + endpoints for cases, deaths, recoveries

---
+++ consts.js
@@ -0,0 +1,19 @@
+let todaysDate = new Date().toLocaleDateString();
+let confirmedCasesUrl =
+  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv';
+let confirmedDeathsUrl =
+  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv';
+let confirmedRecoveriesUrl =
+  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv';
+let confirmedCases = `covid19-Cases-${todaysDate}.csv`;
+let confirmedDeaths = `covid19-Deaths-${todaysDate}.csv`;
+let confirmedRecoveries = `covid19-Recoveries-${todaysDate}.csv`;
+
+module.exports = {
+  confirmedCasesUrl,
+  confirmedDeathsUrl,
+  confirmedRecoveriesUrl,
+  confirmedCases,
+  confirmedDeaths,
+  confirmedRecoveries
+};
--- server.js
+++ server.js
@@ -1,35 +1,54 @@
 const express = require('express');
 const csv = require('csvtojson');
 var schedule = require('node-schedule');
-
 let { download } = require('./utils');
-
 const app = express();
 const port = 3000;
 
-var url =
-  'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv';
-var dest = 'covid19.csv';
+let {
+  confirmedCasesUrl,
+  confirmedDeathsUrl,
+  confirmedRecoveriesUrl,
+  confirmedCases,
+  confirmedDeaths,
+  confirmedRecoveries
+} = require('./consts');
 
-
-var job = schedule.scheduleJob('01 10 * * *', function(){
+var job = schedule.scheduleJob('01 10 * * *', function() {
   console.log('Fetching new data for confirmed Covid-19 cases.');
-  download(url, dest, data => {});
+  download(confirmedCasesUrl, confirmedCases, data => {});
+  download(confirmedDeathsUrl, confirmedDeaths, data => {});
+  download(confirmedRecoveriesUrl, confirmedRecoveries, data => {});
 });
 
-let csvData = (async () => {
-  var jsons = await csv().fromFile(dest);
-  // console.log(jsons);
+// let csvData = (async fileName => {
+//   var jsons = await csv().fromFile(fileName);
+//   // console.log(jsons);
+//   return jsons;
+// })().catch(err => {
+//   console.log(err);
+// });
+
+let csvData = async fileName => {
+  var jsons = await csv().fromFile(fileName);
   return jsons;
-})().catch(err => {
-  console.log(err);
-});
+};
 
-app.get('/data', async (req, res) => {
-  const data = await csvData;
+app.get('/cases', async (req, res) => {
+  const data = await csvData(confirmedCases);
+  res.send(data);
+});
+app.get('/deaths', async (req, res) => {
+  const data = await csvData(confirmedDeaths);
+  res.send(data);
+});
+app.get('/recoveries', async (req, res) => {
+  const data = await csvData(confirmedRecoveries);
   res.send(data);
 });
 app.listen(port, () =>
   console.log(`Application listening on port ${port}.
-Covid-19 data served on port ${port}/data.`)
+Covid-19 Confirmed Cases data served on port ${port}/cases.
+Covid-19 Confirmed Deaths data served on port ${port}/deaths.
+Covid-19 Confirmed Recoveries data served on port ${port}/recoveries.`)
 );
