ffb2b311d2a2087ea0cd0832e8752518ec287170
Anders Svensson <andess86@gmail.com>
2020-03-14 11:32:07 +0100

    Covid-19 data exposed with express endpoint

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
+const express = require('express');
 const csv = require('csvtojson');
 let { download } = require('./utils');
+
+const app = express();
+const port = 3000;
 
 var url =
   'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv';
 var dest = 'covid19.csv';
 
-download(url, dest, data => {});
+// download(url, dest, data => {});
 
-(async () => {
+let csvData = (async () => {
   var jsons = await csv().fromFile(dest);
-  console.log(jsons);
+  // console.log(jsons);
   return jsons;
 })().catch(err => {
   console.log(err);
 });
+
+app.get('/data', async (req, res) => {
+  const data = await csvData;
+  res.send(data);
+});
+app.listen(port, () =>
+  console.log(`Application listening on port ${port}.
+Covid-19 data served on port ${port}/data.`)
+);
