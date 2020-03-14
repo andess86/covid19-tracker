51b1ac7d1f8c56f9e13fbcfde5f60e0988086af0
Anders Svensson <andess86@gmail.com>
2020-03-14 12:54:59 +0100

    Fix heroku issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const csv = require('csvtojson');
 var schedule = require('node-schedule');
 let { download } = require('./utils');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 let {
   confirmedCasesUrl,
