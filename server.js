906951a9ae22cb6cdd099b8357073ff36d283051
Anders Svensson <andess86@gmail.com>
2020-03-14 11:11:52 +0100

    Add function to async read csv to json

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const csv = require('csvtojson');
 let { download } = require('./utils');
 
 var url =
@@ -5,3 +6,11 @@ var url =
 var dest = 'covid19.csv';
 
 download(url, dest, data => {});
+
+(async () => {
+  var jsons = await csv().fromFile(dest);
+  console.log(jsons);
+  return jsons;
+})().catch(err => {
+  console.log(err);
+});
