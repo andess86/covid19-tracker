bdfc8b3a81d2ce994a018488a4ede25fa59a79a2
Anders Svensson <andess86@gmail.com>
2020-03-16 17:12:52 +0100

    Comment out cronjob for now

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+const request = require('request');
 const express = require('express');
 const csv = require('csvtojson');
-var schedule = require('node-schedule');
-let { download } = require('./utils');
+// var schedule = require('node-schedule');
+// let { download } = require('./utils');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -9,35 +10,37 @@ let {
   confirmedCasesUrl,
   confirmedDeathsUrl,
   confirmedRecoveriesUrl,
-  confirmedCases,
-  confirmedDeaths,
-  confirmedRecoveries
+  // confirmedCases,
+  // confirmedDeaths,
+  // confirmedRecoveries
 } = require('./consts');
 
-var job = schedule.scheduleJob('* 12 * * *', function() {
-  console.log('Fetching new data for confirmed Covid-19 cases.');
-  download(confirmedCasesUrl, confirmedCases, data => {});
-  download(confirmedDeathsUrl, confirmedDeaths, data => {});
-  download(confirmedRecoveriesUrl, confirmedRecoveries, data => {});
-});
 
-let csvData = async fileName => {
-  var jsons = await csv().fromFile(fileName);
+//TODO - store in mongoDB atlas / aws dynamo
+// var job = schedule.scheduleJob('* 12 * * *', function() {
+//   console.log('Fetching new data for confirmed Covid-19 cases.');
+//   download(confirmedCasesUrl, confirmedCases, data => {});
+//   download(confirmedDeathsUrl, confirmedDeaths, data => {});
+//   download(confirmedRecoveriesUrl, confirmedRecoveries, data => {});
+// });
+
+let getCsvFromGithub = async file => {
+  var jsons = await csv().fromStream(request.get(file));
   return jsons;
 };
 
 app.get('/cases', async (req, res) => {
-  const data = await csvData(confirmedCases);
-  res.send(data);
+  res.send(await getCsvFromGithub(confirmedCasesUrl));
 });
+
 app.get('/deaths', async (req, res) => {
-  const data = await csvData(confirmedDeaths);
-  res.send(data);
+  res.send(await getCsvFromGithub(confirmedDeathsUrl));
 });
+
 app.get('/recoveries', async (req, res) => {
-  const data = await csvData(confirmedRecoveries);
-  res.send(data);
+  res.send(await getCsvFromGithub(confirmedRecoveriesUrl));
 });
+
 app.listen(port, () =>
   console.log(`Application listening on port ${port}.
 Covid-19 Confirmed Cases data served on port ${port}/cases.
