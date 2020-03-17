bbc910cc05370235095464f7f6c2a285bafcde7d
Anders Svensson <andess86@gmail.com>
2020-03-18 00:31:55 +0100

    Move function to util file

--- utils.js
+++ utils.js
@@ -1,28 +1,29 @@
 const fs = require('fs');
 const request = require('request');
+const csv = require('csvtojson');
+
+let getCsvFromGithub = async file => {
+  var jsons = await csv().fromStream(request.get(file));
+  return jsons;
+};
 
 const download = (url, dest, cb) => {
   const file = fs.createWriteStream(dest);
   const sendReq = request.get(url);
-
   // verify response code
   sendReq.on('response', response => {
     if (response.statusCode !== 200) {
       return cb('Response status was ' + response.statusCode);
     }
-
     sendReq.pipe(file);
   });
-
   // close() is async, call cb after close completes
   file.on('finish', () => file.close(cb));
-
   // check for request errors
   sendReq.on('error', err => {
     fs.unlink(dest);
     return cb(err.message);
   });
-
   file.on('error', err => {
     // Handle errors
     fs.unlink(dest); // Delete the file async. (But we don't check the result)
@@ -30,4 +31,4 @@ const download = (url, dest, cb) => {
   });
 };
 
-module.exports = { download };
+module.exports = { download, getCsvFromGithub };
